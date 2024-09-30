865e71d0bfa6d51bf6d3e95bad2e6b8adfae48fb
Turtle <gustavsson.sofia@hotmail.com>
2024-09-30 11:34:41 +0200

    add basic setup with stack navigation, between 2 screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export function SecondScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>🐛🐛🐛🐛🐛🐛🐛🐛</Text>
+    </View>
+  );
+}
+
+export function HomeScreen({ navigation }: any) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home! 🐛</Text>
+      <StatusBar style="auto" />
+      <Button title="To Second" onPress={() => navigation.navigate("Second")} />
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Home! 🐛</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Second" component={SecondScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +40,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
   },
 });
